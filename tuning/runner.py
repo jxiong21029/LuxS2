863256001a189d37efe9829cc2a2cd8dae668f2b
BrownieInMotion <accounts@danielclu.com>
2023-04-22 18:41:21 -0400

    feat: add extra parameters for tuning

--- tuning/runner.py
+++ tuning/runner.py
@@ -4,7 +4,7 @@ import torch
 import zarr
 from torch.utils.data import DataLoader
 
-from model import LuxAIModel
+from model import LuxAIModel, UNet
 from train import LuxAIDataset, Trainer
 from tuning.logger import Logger
 from tuning.tuning import Tuner
@@ -14,14 +14,23 @@ LOADER_WORKERS = 1
 EPOCHS = 4
 
 
+# extraneous parmeter slots
+# useful for model-specific params
+extras = {
+    "extra_1": "nuisance",
+    "extra_2": "nuisance",
+    "extra_3": "nuisance",
+}
+
+
 def trial(config):
-    missing = {"lr", "weight_decay", "batch_size"} - config.keys()
+    missing = (
+        ({"lr", "weight_decay", "batch_size"} | extras.keys()) - config.keys()
+    )
     assert not missing, f"missing keys: {missing}"
 
     array = zarr.open(DATASET)
-    # for testing
-    array["action_amounts"] = array["action_amounts"][:1000]
-    data = LuxAIDataset(array)
+    data = LuxAIDataset(array, max_size=1000)
     loader = DataLoader(
         data,
         batch_size=config["batch_size"],
@@ -30,7 +39,22 @@ def trial(config):
         drop_last=True,
         num_workers=LOADER_WORKERS,
     )
-    network = LuxAIModel()
+
+    kwargs = {
+        name: value
+        for (name, value) in (
+            (None, None)
+            if config[key] is None
+            else config[key]
+            for key in extras.keys()
+        )
+        if value is not None
+    }
+
+    print(config["model"], kwargs)
+
+    network = config["model"](**kwargs)
+
     logger = Logger()
     trainer = Trainer(
         loader,
@@ -49,28 +73,55 @@ def trial(config):
 
 def main():
     spec = {
+        "model": "science",
         "batch_size": "nuisance",
         "lr": "nuisance",
-        "weight_decay": "science",
+        "weight_decay": "nuisance",
     }
 
     tuner = Tuner(
-        spec,
+        spec | extras,
         trial_fn=trial,
         metric="loss",
         mode="min",
         throw_on_exception=True,
         trial_gpus=1,
     )
+
+    models = {
+        "unet": (UNet, {"extra_1": ("out", [20, 30, 40])}),
+        "linear": (LuxAIModel, {}),
+    }
+
     params = {
         "batch_size": [16],
         "lr": [1e-3, 1e-2],
         "weight_decay": [1e-5],
     }
 
-    for config in itertools.product(*params.values()):
-        config = dict(zip(params.keys(), config))
-        tuner.add(config)
+    for _, (model, options) in models.items():
+        # i.e. if models = {'linear': (A, {'extra_1': ('param', [1, 2])})}
+        # we want to produce
+        # {
+        #     'extra_1': [('param', 1), ('param', 2)]
+        #     'extra_2': [None]
+        #     'extra_3': [None]
+        # }
+        additional = {
+            key: [None] if name is None else [
+                (name, choice)
+                for choice in choices
+            ]
+            for key, (name, choices) in (
+                (key, options.get(key, (None, [])))
+                for key in extras.keys()
+            )
+        }
+
+        all_params = params | additional
+        print(all_params)
+        for options in itertools.product(*all_params.values()):
+            tuner.add(dict(zip(all_params.keys(), options)) | {"model": model})
 
     tuner.run()
 
